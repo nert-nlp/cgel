52b859381720612e35a0352df810008bb183866c
Aryaman Arora <aarora24@apple.com>
2022-06-14 17:33:24 -0700

    improvements to CGEL class; cleaning CGEL data for alignment

--- analysis/pos.py
+++ analysis/pos.py
@@ -4,6 +4,7 @@ sys.path.append('../')
 import cgel
 from collections import Counter
 from math import log
+from difflib import get_close_matches
 
 with open('../datasets/cgel_from_ud/ud_train.conllu') as f:
     ud_data = conllu.parse(f.read())
@@ -11,12 +12,25 @@ with open('../datasets/cgel_from_ud/ud_train.conllu') as f:
 with open('../datasets/cgel_from_ud/cgel.trees.conllu') as f:
     cgel_data = conllu.parse(f.read())
 
+trees = []
+with open('../datasets/cgel_from_ud/training_set.txt') as f:
+    for tree in cgel.parse(''.join([x for x in f.readlines() if x[0] in [' ', '(']])):
+        trees.append(conllu.parse(tree.to_conllu())[0])
+
 cgel, ud, penn = Counter(), Counter(), Counter()
 cgel_ud, ud_penn, penn_cgel = Counter(), Counter(), Counter()
 
 tot = 0
-for ud_tree, cgel_tree in zip(ud_data, cgel_data):
-    for ud_tok, cgel_tok in zip(ud_tree, cgel_tree):
+for ud_tree, cgel_tree in zip(ud_data, trees):
+    j = 0
+    for cgel_tok in cgel_tree:
+        print(cgel_tok)
+        while not get_close_matches(cgel_tok['form'].lower(), [ud_tree[j]['form'].lower()]):
+            print(cgel_tok['form'], [ud_tree[j]['form']])
+            j += 1
+        
+        ud_tok = ud_tree[j]
+        print(ud_tok, cgel_tok)
         ud_pos = ud_tok['upos']
         penn_pos = ud_tok['xpos']
         cgel_pos = cgel_tok['upos']
@@ -28,6 +42,7 @@ for ud_tree, cgel_tree in zip(ud_data, cgel_data):
         ud_penn[(ud_pos, penn_pos)] += 1
         penn_cgel[(penn_pos, cgel_pos)] += 1
         tot += 1
+        j += 1
 
 def H(c):
     res = 0
@@ -35,14 +50,32 @@ def H(c):
         res -= freq / tot * log(freq / tot, 2)
     return res
 
-mi_cgel_ud = 0
+def cond_H(count, given, pos):
+    return -(count / tot) * log((count / tot) / (given[pos] / tot), 2)
+
+cond_cgel_ud = 0
+res = Counter()
 for (cgel_pos, ud_pos), count in cgel_ud.items():
-    mi_cgel_ud += (count / tot) * log((count * tot) / (cgel[cgel_pos] * ud[ud_pos]), 2)
-mi_ud_penn = 0
-for (ud_pos, penn_pos), count in ud_penn.items():
-    mi_ud_penn += (count / tot) * log((count * tot) / (ud[ud_pos] * penn[penn_pos]), 2)
-mi_penn_cgel = 0
+    x = cond_H(count, ud, ud_pos)
+    cond_cgel_ud += x
+    if x != 0:
+        res[(cgel_pos, ud_pos)] = x
+for x, y in res.most_common():
+    print(x, y)
+print()
+
+cond_cgel_penn = 0
+res = Counter()
 for (penn_pos, cgel_pos), count in penn_cgel.items():
-    mi_penn_cgel += (count / tot) * log((count * tot) / (penn[penn_pos] * cgel[cgel_pos]), 2) 
-print(mi_cgel_ud, mi_ud_penn, mi_penn_cgel)
-print(H(cgel), H(ud), H(penn))
+    x = cond_H(count, penn, penn_pos)
+    cond_cgel_penn += x
+    if x != 0:
+        res[(cgel_pos, penn_pos)] = x
+for x, y in res.most_common():
+    print(x, y)
+
+print(f'H(CGEL) = {H(cgel)} ({len(cgel)})')
+print(f'H(UD)   = {H(ud)} ({len(ud)})')
+print(f'H(Penn) = {H(penn)} ({len(penn)})')
+print(f'H(CGEL | UD)   = {cond_cgel_ud}')
+print(f'H(CGEL | Penn) = {cond_cgel_penn}')
--- cgel.py
+++ cgel.py
@@ -1,19 +1,34 @@
+#!/usr/bin/env python3
+"""
+Library for parsing and manipulating CGEL trees in machine-readable
+format, exposing useful helper functions.
+
+@author: Aryaman Arora (@aryamanarora)
+"""
+
 from collections import defaultdict
+import re
+from enum import Enum
 
 class Node:
     def __init__(self, deprel, constituent, head, text=None):
         self.deprel = deprel
-        self.constituent = constituent
+        if '_' in constituent:
+            self.constituent, self.label = constituent.split('_')
+        else:
+            self.constituent = constituent
+            self.label = None
         self.text = text
         self.head = head
     
     def __str__(self):
+        cons = self.constituent + (self.label if self.label else '')
         if self.text:
-            return f':{self.deprel} ({self.constituent} :t "{self.text}"'
+            return f':{self.deprel} ({cons} :t "{self.text}"'
         elif self.deprel:
-            return f':{self.deprel} ({self.constituent}'
+            return f':{self.deprel} ({cons}'
         else:
-            return f'({self.constituent}'
+            return f'({cons}'
 
 class Tree:
     def __init__(self):
@@ -85,6 +100,85 @@ class Tree:
                 return
         for i in self.children[cur]:
             self.merge_text_rec(i, string)
+
+    def to_conllu(self) -> str:
+        result = ""
+        count = 1
+        for key, token in dict(sorted(self.tokens.items())).items():
+            if token.text and token.constituent != 'GAP':
+                result += '\t'.join([str(count), token.text, '_', token.constituent, '_', '_', '_', '_', '_', '_']) + '\n'
+                count += 1
+        return result
     
     def __str__(self):
         return self.draw().strip()
+
+class State(Enum):
+    NONE = 0
+    NODE = 1
+    EDGE = 2
+    PROPERTY = 3
+    TEXT = 4
+    OPEN_PAREN = 5
+    CLOSE_PAREN = 6
+
+def parse(s):
+    s = s.replace('\n', ' ')
+    
+    tokens = []
+    token = ""
+    status = State.NONE
+    for char in s:
+        if char == '(' and status in [State.NONE, State.EDGE]:
+            if token: tokens.append((token.strip(), status))
+            tokens.append(('(', State.OPEN_PAREN))
+            token = ''
+            status = State.NODE
+        elif char == ':' and status in [State.NODE, State.NONE]:
+            tokens.append((token.strip(), status))
+            token = ''
+            status = State.EDGE
+        elif char == '"' and status in [State.EDGE]:
+            tokens.append((token.strip(), status))
+            token = ''
+            status = State.TEXT
+        elif char == '"' and status in [State.TEXT]:
+            tokens.append((token.strip(), status))
+            token = ''
+            status = State.NONE
+        elif char == ')' and status in [State.NODE, State.NONE]:
+            if status == State.NODE: tokens.append((token.strip(), status))
+            tokens.append((')', State.CLOSE_PAREN))
+            token = ''
+            status = State.NONE
+        elif status in [State.NODE, State.EDGE, State.TEXT]:
+            token += char
+            
+    res = []
+    result = None
+    stack = []
+    count = 0
+    edge = None
+    d = 0
+    for token, state in tokens:
+        if state == State.OPEN_PAREN:
+            d += 1
+        elif state == State.NODE:
+            assert len(stack) == d - 1
+            if stack: result.add_token(None, edge, token, count, stack[-1][1])
+            else:
+                if result: res.append(result)
+                result = Tree()
+                result.add_token(None, '', token, count, -1)
+            stack.append((token, count))
+            count += 1
+        elif state == State.EDGE:
+            edge = token
+        elif state == State.TEXT:
+            result.add_token(token, None, None, count, stack[-1][1])
+            count += 1
+        elif state == State.CLOSE_PAREN:
+            d -= 1
+            stack.pop()
+    if result: res.append(result)
+    return res
