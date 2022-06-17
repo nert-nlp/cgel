e85f00cd0648ca527700cab2a795363a184a2930
aryamanarora <aryaman.arora2020@gmail.com>
2022-06-17 15:23:22 -0700

    stats for functions/constituents, fixes to functions (see #16)

---
+++ analysis/nodes.py
@@ -0,0 +1,25 @@
+import conllu
+import sys
+sys.path.append('../')
+import cgel
+from collections import Counter
+from math import log
+from difflib import get_close_matches
+
+with open('../datasets/twitter_ud.conllu') as f, open('../datasets/ewt_ud.conllu') as f2:
+    ud_data = conllu.parse(f.read() + f2.read())
+
+trees = []
+with open('../datasets/twitter_cgel.txt') as f, open('../datasets/ewt_cgel.txt') as f2:
+    a = ''.join([x for x in f.readlines() + f2.readlines() if x[0] in [' ', '(']])
+    for tree in cgel.parse(a):
+        trees.append(tree)
+
+function, constituent = Counter(), Counter()
+for tree in trees:
+    for node in tree.tokens.values():
+        function[node.deprel] += 1
+        constituent[node.constituent] += 1
+
+for i in function.most_common():
+    print(i)
