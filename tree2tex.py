a820e81f9cef26b580c6ee8752303dc8204245d9
Nathan Schneider <nathan.schneider@georgetown.edu>
2022-07-30 00:15:49 -0400

    tree2tex conversion

--- tree2tex.py
+++ tree2tex.py
@@ -32,20 +32,26 @@ r'''
 )))
 '''
 
-QTD = r'"([^"\\]|\\"|\\\\)+"'
+QTD = r'"(?:[^"\\]|\\"|\\\\)+"'
 def cgel2tex(s: str) -> str:
-    t = re.sub(r'^\((\S+)', r'(.\1.', s)
+    t = re.sub(r' (:p|:l|:subt|:subp|:note) '+QTD, '', s)   # remove punctuation, lemmas, subtokens, notes
+    t = t.replace('N_pro', r'N\textsubscript{\textsc{pro}}')
+    t = t.replace('V_aux', r'V\textsubscript{\textsc{aux}}')
+    t = t.replace('Clause_rel', r'Clause\textsubscript{rel}')
+    t = t.replace('Obj_dir', r'Obj\textsubscript{dir}')
+    t = t.replace('Obj_ind', r'Obj\textsubscript{ind}')
+    t = t.replace('Comp_ind', r'Comp\textsubscript{ind}')
+    assert '_' not in t,t
+    t = re.sub(r'^\((\S+)', r'(.\1.', t)    # root
     t = re.sub(r'(\s+):(\S+) \((\S+) / GAP',
-                r'\1:\2 (\3 / GAP :t "--"', t)
+                r'\1:\2 (\3 / GAP :t "--"', t)  # add -- terminal for gap
     t = re.sub(r'(\s+):(\S+) \((\S+) / ([^\s\)]+)',
-                r'\1:\2 (\4\\textsubscript{\3}', t)
+                r'\1:\2 (\4\\textsubscript{\3}', t) # coindexation subscript
     t = re.sub(r'(\s+):(\S+) \(([^\s\)]+)',
-                #r'\1(.\\NL{\2}{\3}.', t)
-                lambda m: (m.group(1)+r'(.\NL{'+m.group(2)+'}{' +
-                    re.sub(r'(\S+)_([A-Za-z]+)', r'\1\\textsubscript{\\textsc{\2}}', m.group(3)) + '}.'),
-                t)
-    t = re.sub(r' (:p|:l|:subt) '+QTD, '', t)
-    t = re.sub(r' :t ('+QTD+')',
+                r'\1(.\\NL{\2}{\3}.', t)    # non-root nonterminal
+    t = re.sub(r'( :t ('+QTD+'))? :correct ('+QTD+')',    # token with correction
+                lambda m: "  `"+latex_quote(cgel.cgel_unquote(m.group(2) or '""') + ' [' + cgel.cgel_unquote(m.group(3)) + ']') + "'", t)
+    t = re.sub(r' :t ('+QTD+')',    # token
                 lambda m: "  `"+latex_quote(cgel.cgel_unquote(m.group(1)))+"'", t)
     return SHEADER + t + SFOOTER
 
@@ -86,5 +92,5 @@ if __name__=='__main__':
             s = tree.draw()
             print(cgel2tex(s))
             i += 1
-            if i>=2: break
+            #if i>=2: break
         print(FOOTER)
