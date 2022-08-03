801b76a0f3af0f81ce80b155d18fe5f8d5e0d507
Nathan Schneider <nathan.schneider@georgetown.edu>
2022-08-03 00:22:12 -0400

    extract_grammar.py: filtering to instances of suspicious rules

--- extract_grammar.py
+++ extract_grammar.py
@@ -1,6 +1,7 @@
 """
 Create a grammar with rules attested in the corpus.
-Options control refinement of nonterminals.
+
+TODO: Options control refinement of nonterminals. E.g. clause types, marked coordinate categories
 
 @author: Nathan Schneider (@nschneid)
 @since: 2022-07-30
@@ -14,12 +15,18 @@ import cgel
 def count_rules(i, node, tree, counts, examples, select_rules=set(), opts=set()):
     """
     Options to provide in `opts`:
-        'heads-only': In the RHS of a rule only display items in the Head function
+        'heads-only': In the RHS of a rule only display items in the Head function.
+        'ignore-supp': In the RHS of a rule, omit items in the Supplement function.
+        'group-cats': In the RHS of a rule, group together all constituent categories under each function.
     """
     assert tree.tokens[i] is node
     children = [(c,tree.tokens[c]) for c in tree.children[i]]
     if children:
-        rhs = ' '.join(f'{ch.deprel}:{ch.constituent}' for ch in list(zip(*children))[1] if 'Head' in ch.deprel or 'heads-only' not in opts)
+        rhsCondition = lambda x: ('Head' in x.deprel or 'heads-only' not in opts) and (x.deprel!='Supplement' or 'ignore-supp' not in opts)
+        rhs = ' '.join(f'{ch.deprel}:{ch.constituent}' for ch in list(zip(*children))[1] if rhsCondition(ch))
+        if 'group-cats' in opts:
+            fullrhs = rhs
+            rhs = ' '.join(f'{ch.deprel}:*' for ch in list(zip(*children))[1] if rhsCondition(ch))
         rule = f'{node.constituent} -> {rhs}'
         if not select_rules or rule in select_rules:
             counts[rule] += 1
@@ -34,6 +41,33 @@ def count_rules(i, node, tree, counts, examples, select_rules=set(), opts=set())
 if __name__=='__main__':
     rules = Counter()
     examples = defaultdict(set)
+
+    SELECTED_RULES = {  # heads-only rules
+                #'Clause -> ',
+                'Clause -> Head:V',
+                'DP -> Head:Flat',
+                'NP -> Det-Head:DP',
+                'NP -> Det-Head:DP Head:GAP',
+                'NP -> Det-Head:NP Head:Nom',
+                'NP -> Head-Prenucleus:NP Head:Nom',
+                'Nom -> Head:NP',
+                'VP -> Head:V Head:Coordination'}
+    # SELECTED_RULES = { # 'PP -> Head:P PredComp:NP', # as-PPs
+    #     #'PP -> Comp:NP Head:PP', # NPN construction: 'back to back'
+    #     'PP -> Head:P Mod:PP Mod:PP'}
+    #SELECTED_RULES |= {'VP -> Head:VP Mod:DP','VP -> Head:V_aux Comp:NP'}
+    # SELECTED_RULES = {'NP -> Head-Prenucleus:NP Head:Nom', 'NP -> Head:Nom Mod:Clause_rel',
+    #     'NP -> Head:Nom Supplement:VP', 'NP -> Mod:AdjP Head:NP', 'NP -> Mod:AdjP Head:Nom'}
+    #
+    SELECTED_RULES = {'VP -> Head:* Mod:* Comp:*','VP -> Head:* Comp:* Mod:*','VP -> Head:* Obj:* Mod:*',
+        'VP -> Head:* PredComp:* Mod:*', 'VP -> Mod:* Head:* Obj:*', 'VP -> Head:* Mod:* PredComp:*', 'VP -> Head:* Subj:*',
+        'VP -> Head:* DisplacedSubj:*', 'VP -> Mod:* Head:* Comp:*',
+        'VP -> Head:* DisplacedSubj:* Mod:*', 'VP -> Marker:* Head:* Mod:*'}
+
+    # TODO: examine Clause -> * rules
+
+    #SELECTED_RULES |= {'AdjP -> Head:Adj Mod:PP'}
+    #SELECTED_RULES = set()
     with open('datasets/twitter.cgel') as f, open('datasets/ewt.cgel') as f2:
         i = 0
         for tree in cgel.trees(chain(f,f2)):
@@ -43,32 +77,16 @@ if __name__=='__main__':
             rule = f'ROOT -> {root.constituent}'
             rules[rule] += 1
             count_rules(r, root, tree, rules, examples,
-                select_rules={'AdvP -> Head:P',
-                            #'Clause -> ',
-                            'Clause -> Head:V',
-                            'DP -> Head:Flat',
-                            'DP -> Head:N_pro',
-                            'NP -> Det-Head:DP',
-                            'NP -> Head:N',
-                            'NP -> ',
-                            'NP -> Det-Head:DP Head:GAP',
-                            'NP -> Det-Head:NP Head:Nom',
-                            'NP -> Head-Prenucleus:NP Head:Nom',
-                            'NP -> Head:GAP',
-                            'NP -> Head:N_pro',
-                            'NP -> Head:Nom Head:VP',
-                            'NP -> Head:VP',
-                            'Nom -> Head:NP',
-                            'Nom -> Head:Adj',
-                            'Nom -> Head:PP',
-                            'VP -> ',
-                            'VP -> Head:Clause',
-                            'VP -> Head:V Head:Coordination',
-                            'VP -> Head:V Head:VP'},
-                opts={'heads-only'})
+                select_rules=SELECTED_RULES,
+                opts={'heads-only', 'group-cats'} and {'ignore-supp'} and {'group-cats','ignore-supp'})
 
     for k,v in sorted(rules.items(), key=lambda x: (x[0].split()[0], -x[1], x[0].split()[1:])):
         print(f'{v:3}', k)
-        for ex in examples[k]:
-            print(ex)
-        print()
+        if 'print examples' and SELECTED_RULES:
+            for ex in examples[k]:
+                print(ex)
+            print()
+
+    for rule in SELECTED_RULES:
+        if rules[rule]==0:
+            print(f'Unattested: {rule}')
