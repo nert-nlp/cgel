75efa043964ff41f6808814f4b1b2d4b9bd81295
Nathan Schneider <nathan.schneider@georgetown.edu>
2022-08-26 23:51:04 -0400

    tree2tex: now just a wrapper for cgel.py conversion; remove support for parsetree package

--- tree2tex.py
+++ tree2tex.py
@@ -1,7 +1,6 @@
 """
-Convert a CGEL tree to LaTeX.
-
-Not yet supported: fused heads
+Convert a CGEL tree to LaTeX (forest package).
+Most of the implementation is in cgel.py.
 
 @author: Nathan Schneider (@nschneid)
 @since: 2022-07-29
@@ -9,31 +8,7 @@ Not yet supported: fused heads
 
 import re, sys
 from itertools import chain
-from pylatexenc.latexencode import unicode_to_latex
 import cgel
-
-def latex_quote(s, dialect):
-    if dialect=='parsetree':
-        return unicode_to_latex(s).replace("'", r'\textquoteright{}').replace("`", r'\textquoteleft{}')
-    else:
-        return unicode_to_latex(s).replace(',', '{,}')  # forest package doesn't like unescaped commas in terminals for some reason
-
-# Example tree
-r'''
-(.Clause.
-    (.\NL{Subj}{NP}.
-        (.\NL{Head}{Nom}.
-            (.\NL{Head}{N}. `it')))
-    (.\NL{Head}{VP}.
-        (.\NL{Head}{V}. `\textquoteright s' )
-        (.\NL{PredComp}{AdjP}.
-            (.\NL{Head}{Adj}. `alright' )))
-    (.\NL{ExtraposedSubject}{Clause}.
-        (.\NL{Head}{VP}.
-            (.\NL{Marker}{Subordinator}. `to' )
-        (.\NL{Head}{VP}.  `say they\textquoteright re white' )
-)))
-'''
 
 """ fusion (new style):
 (x / NP
@@ -54,67 +29,9 @@ Similarly for Head-Prenucleus (prenucleus follows head) and Mod-Head, Marker-Hea
 		]]
 """
 
-QTD = r'"(?:[^"\\]|\\"|\\\\)*"'
-def cgel2tex(s: str, dialect='forest' or 'parsetree') -> str:
-    t = re.sub(r' (:p|:l|:subt|:subp|:note) '+QTD, '', s)   # remove punctuation, lemmas, subtokens, notes
-    t = t.replace('N_pro', r'N\textsubscript{pro}')
-    t = t.replace('V_aux', r'V\textsubscript{aux}')
-    t = t.replace('Clause_rel', r'Clause\textsubscript{rel}')
-    t = t.replace('Obj_dir', r'Obj\textsubscript{dir}')
-    t = t.replace('Obj_ind', r'Obj\textsubscript{ind}')
-    t = t.replace('Comp_ind', r'Comp\textsubscript{ind}')
-    assert '_' not in t,t
-    t = re.sub(r'\bGAP(?=\))',
-                r'GAP :t "--"', t)  # add -- terminal for gap
-    if 'GAP' in s:
-        assert '--' in t,s
-    t = re.sub(r'([^\s"\)])(?=\)+($|\n))',
-                r'\1 :t ""', t)  # other childless nonterminal with no string (due to fusion in old-style trees)
-    if dialect=='parsetree':
-        t = re.sub(r'(\s+):(\S+) \((\S+) / ([^\s\)]+)',
-                    r'\1:\2 (\4\\textsubscript{\3}', t) # coindexation subscript
-        t = re.sub(r'^\((\S+)', r'(.\1.', t)    # root
-        t = re.sub(r'(\s+):(\S+) \(([^\s\)]+)',
-                    r'\1(.\\NL{\2}{\3}.', t)    # non-root nonterminal
-        t = re.sub(r'( :t ('+QTD+'))? :correct ('+QTD+')',    # token with correction
-                    lambda m: "  `"+latex_quote(cgel.cgel_unquote(m.group(2) or '""') + ' [' + cgel.cgel_unquote(m.group(3)) + ']', dialect) + "'", t)
-        t = re.sub(r' :t ('+QTD+')',    # token
-                    lambda m: "  `"+latex_quote(cgel.cgel_unquote(m.group(1)), dialect)+"'", t)
-    else:
-        t = re.sub(r'(\s+):(\S+) \((\S+) / ([^\s\)]+)',
-                    r'\1:\2 (\4\\idx{\3}', t) # coindexation subscript
-        u = re.sub(r'\)(?=\]*($|\n))', ']', t)  # trailing brackets on a line
-        while u!=t:
-            t = u
-            u = re.sub(r'\)(?=\]*($|\n))', ']', t)  # trailing brackets on a line
-        t = re.sub(r'^\((\S+)', r'[\1', t)    # root
-        t = re.sub(r'(\s+):(\S+) \(([^\s\)]+)',
-                    r'\1[\\Node{\2}{\3}', t)    # non-root nonterminal
-        t = re.sub(r'( :t ('+QTD+'))? :correct ('+QTD+')',    # token with correction
-                    lambda m: "["+latex_quote(cgel.cgel_unquote(m.group(2) or '""') + ' (' + cgel.cgel_unquote(m.group(3)) + ')', dialect) + "]", t)
-        t = re.sub(r' :t ('+QTD+')',    # token
-                    lambda m: "["+latex_quote(cgel.cgel_unquote(m.group(1)), dialect)+"]", t)
-    return SHEADER[dialect] + t + SFOOTER[dialect]
 
-# header for parsetree dialect
-PHEADER = r'''
-\documentclass[12pt]{standalone}
-\usepackage{times}
-\usepackage{parsetree}
-\usepackage{textcomp}
-\pagestyle{empty}
-%----------------------------------------------------------------------
-% Node labels in CGEL trees are defined with \NL, which is defined so that
-% \NL{Abcd}{Xyz} yields a label with the function Abcd on the top, in
-% sanserif font, followed by a colon, and the category Xyz on the bottom.
-\newcommand{\NL}[2]{\begin{tabular}[t]{c}\small\textsf{#1:}\\
-#2\end{tabular}}
-%----------------------------------------------------------------------
-\begin{document}
-'''
-
-# header for forest dialect
-FHEADER = r'''
+# file header -- forest package
+HEADER = r'''
 \documentclass[tikz,border=12pt]{standalone}
 \usepackage[linguistics]{forest}
 \usepackage{times}
@@ -153,38 +70,19 @@ FHEADER = r'''
 \begin{document}
 '''
 
-SHEADER = {'parsetree': r'''
-\begin{parsetree}
-''', 'forest': r'''
-\begin{forest}
-where n children=0{% for each terminal node
-    font=\itshape, 			% italics
-    tier=word          			% align at the "word" tier (bottom)
-  }{%								% no false conditions, so empty
-  },
-'''}
-
-SFOOTER = {'parsetree': r'''
-\end{parsetree}
-''', 'forest': r'''
-\end{forest}
-'''}
-
 FOOTER = '''
 \end{document}
 '''
 
 if __name__=='__main__':
-    dialect = 'forest'
     with open('datasets/twitter.cgel') as f, open('datasets/ewt.cgel') as f2:
         i = 0
-        print(PHEADER if dialect=='parsetree' else FHEADER)
+        print(HEADER)
         for tree in cgel.trees(chain(f,f2)):
             s = tree.draw()
             if i>=46:
                 for k,v in tree.metadata.items():
                     print(f'% # {k} = {v}')
-                #print(cgel2tex(s, dialect=dialect))
                 print(tree.drawtex())
                 break # just print one tree
             i += 1
