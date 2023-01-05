c7f3bfa694767e630dfd4daf70942ec4e78e012b
Nathan Schneider <nathan.schneider@georgetown.edu>
2023-01-04 20:12:51 -0500

    cgel2ptb.py

--- cgel2ptb.py
+++ cgel2ptb.py
@@ -6,7 +6,7 @@ from math import log
 from difflib import get_close_matches
 from itertools import chain
 
-MODE = ['tags', 'trees'][0]
-with open('datasets/twitter.cgel') as f, open('datasets/ewt.cgel') as f2:
-    for tree in cgel.trees(chain(f2,)):
-        print(*tree.tagging(gap_symbol='_.').split() if MODE=='tags' else tree.ptb(), sep='\n')
+MODE = ['tags', 'trees'][1]
+with open('datasets/twitter.cgel') as f, open('datasets/ewt.cgel') as f2, open('datasets/ewt-new1-nschneid.cgel') as f3:
+    for tree in cgel.trees(chain(f,f2,f3)):
+        print(tree.tagging(gap_symbol='_.') if MODE=='tags' else tree.ptb())
