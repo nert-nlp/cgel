d3edc98e1974373718927b94e707bed81f059ff7
Nathan Schneider <nathan.schneider@georgetown.edu>
2023-03-05 17:22:01 -0500

    eval.py: prec vs. recall denominators

--- eval.py
+++ eval.py
@@ -16,8 +16,8 @@ def edit_distance(tree1: Tree, tree2: Tree) -> int:
     for i, spans in enumerate([span1, span2]):
         for span in spans:
             span_by_bounds[i][(span.left, span.right)].append((span.node.deprel, span.node.constituent))
-    
-    # levenshtein distance operations
+
+    # levenshtein distance operations to edit the 1st tree to match the 2nd tree
     ins, delt = 0, 0
     for bound in set(span_by_bounds[0].keys()) | set(span_by_bounds[1].keys()):
         seq1, seq2 = span_by_bounds[0][bound], span_by_bounds[1][bound]
@@ -28,12 +28,12 @@ def edit_distance(tree1: Tree, tree2: Tree) -> int:
             if op[0] != 'delete': ins += 1
             if op[0] != 'insert': delt += 1
 
-    # precision: how much of tree2 is present in tree1? (n2 - ins) / n1
-    # recall: how much of tree1 is present in tree2? (n1 - del) / n2
+    # precision: how much of tree2 is present in tree1? (n2 - ins) / n2
+    # recall: how much of tree1 is present in tree2? (n1 - del) / n1
     dist = ins + delt
-    prec = (len(span2) - ins) / len(span1)
-    rec = (len(span1) - delt) / len(span2)
-    
+    prec = (len(span2) - ins) / len(span2)
+    rec = (len(span1) - delt) / len(span1)
+
     # return scores
     # normalised: the max distance is if the sets of spans are disjoint, so divide by that
     return {
@@ -63,12 +63,12 @@ def test(gold, pred):
             if res['valid']:
                 for metric in res:
                     avg[metric] += res[metric]
-    
+    # compute macroaverages of valid (string-matched) pairs only
     for metric in avg:
         if metric not in ['valid', 'count']:
-            avg[metric] /= count
+            avg[metric] /= avg['valid']
     avg['count'] = count
-        
+
     print(avg)
 
 def main():
