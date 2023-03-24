4951a32fbd316c0fd04f752522c91bbbadf32fb3
Nathan Schneider <nathan.schneider@georgetown.edu>
2023-03-24 19:55:17 -0400

    validation: allow (AdvP :Mod PP)

--- cgel.py
+++ cgel.py
@@ -650,7 +650,7 @@ class Tree:
                         elif ch.deprel!='Supplement' and 'PP+' not in par.constituent and '+PP' not in par.constituent \
                             and self.head_lemma(c)!='along': # TODO: revisit "along with"
                             assert c_d in {('Nom','Comp'), ('Nom','Comp_ind'), ('VP','Comp'), ('VP','Particle'), ('VP','PredComp'), ('AdjP','Comp'),
-                            ('Nom','Mod'), ('VP','Mod'), ('AdjP','Mod'), ('AdjP','Comp_ind'), ('AdvP','Comp_ind'),
+                            ('Nom','Mod'), ('VP','Mod'), ('AdjP','Mod'), ('AdjP','Comp_ind'), ('AdvP','Mod'), ('AdvP','Comp_ind'),
                             ('Nom','Mod-Head'), # the above
                             ('DP','Comp'),  # [DP more/less/fewer [PP than...]] (p. 432)
                             ('NP','Det'),   # [about 30] seconds
