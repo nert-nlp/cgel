9fd25b84b5eec05eb638f07b55c610e386f4c434
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-04-13 22:32:06 -0400

    tests for tree edit distance

--- test/test_levenshtein.py
+++ test/test_levenshtein.py
@@ -1,6 +1,7 @@
-from eval import levenshtein
+from eval import levenshtein, tree_edit_distance
+from cgel import trees
 
-def test_levenshtein():
+def test_levenshtein_seqs():
     assert levenshtein([('a','a'), ('b','b')], [('a','a'), ('b','b')]) == (0.0, [])
     assert levenshtein([('a','a'), ('b','b')], [('a','a'), ('b','b')], matches=True) == (0.0, [('match', 0, 0), ('match', 1, 1)])
     assert levenshtein([('a','a'), ('b','b')], [('A','A'), ('b','b')]) == (1.0, [('substitute', 0, 0)])
@@ -9,3 +10,11 @@ def test_levenshtein():
     assert levenshtein([('a','a'), ('b','b'), ('c','c')], [('b','b'), ('c','c'), ('d','d')], matches=True) == (2.0, [('delete', 0, 0), ('match', 1, 0), ('match', 2, 1), ('insert', 3, 2)])
     assert levenshtein([('a','a'), ('b','b')], [('c','c'), ('b','b'), ('e','e'), ('f','f')]) == (3.0, [('substitute', 0, 0), ('insert', 2, 2), ('insert', 2, 3)])
     assert levenshtein(['a', 'a'], ['a', 'a', 'a'], matches=True) == (1.0, [('match', 0, 0), ('match', 1, 1), ('insert', 2, 2)])
+
+def test_ted_trees():
+    with open('test/test1.cgel') as f, open('test/test2.cgel') as g:
+        F = [tree for tree in trees(f, check_format=True)]
+        G = [tree for tree in trees(g, check_format=True)]
+
+        for i in range(len(F)):
+            assert tree_edit_distance(F[i], G[i]) == int(G[i].metadata['expected_ted'])
