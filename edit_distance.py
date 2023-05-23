c18ee5ff371fb10468478343d1cdb9db3731bb47
Nathan Schneider <nathan.schneider@georgetown.edu>
2023-05-23 18:20:09 -0400

    new TED implementation: tweak function signature, remove inefficient use of Counter

--- edit_distance.py
+++ edit_distance.py
@@ -1,4 +1,4 @@
-from cgel import Tree, trees, Span
+from cgel import Tree, Node, trees, Span
 from typing import List, Tuple, Mapping
 
 from collections import Counter
@@ -6,7 +6,7 @@ from collections import Counter
 DEBUG = False
 
 def TED(T1: Tree, T2: Tree,
-        INS: float = 1, DEL: float = 1, SUB: float = 1) -> Tuple[float,Counter,Tuple[Tuple[str,str]]]:
+        INS: float = 1, DEL: float = 1, SUB: float = 1) -> Tuple[float,Counter,Mapping[int,int]]:
     """
     TREE EDIT DISTANCE
     Implementation of Milos Simic's pseudocode in <https://www.baeldung.com/cs/tree-edit-distance>,
@@ -25,12 +25,14 @@ def TED(T1: Tree, T2: Tree,
     """
     memo = {}
 
-    nodes1, leftmost1, labels1 = [], [], []
-    nodes2, leftmost2, labels2 = [], [], []
+    nodes1: List[int] = []
+    nodes2: List[int] = []
+    leftmost1, labels1 = [], []
+    leftmost2, labels2 = [], []
     # populate nodes by postorder traversal.
     # labels are what will be compared when two nodes are aligned to determine whether to incur a substitution cost
     # note that any gap antecedent is not included in the label - it will have to be checked after the full alignment is computed by TED
-    def _postorder(n, T, result, leftmosts, labels):
+    def _postorder(n: int, T: Tree, result: List[int], leftmosts: List[int], labels: List[tuple]):
         _leftmost = None
         for c in T.children[n]:
             x = _postorder(c, T, result, leftmosts, labels)
@@ -54,7 +56,7 @@ def TED(T1: Tree, T2: Tree,
         j1: int,    # end of subforest within T1 (rightmost root node + 1)
         i2: int,    # start of subforest within T2
         j2: int,    # end of subforest within T2
-    ) -> Tuple[float,Counter,Tuple[Tuple[int,int]]]:    # cost
+    ) -> Tuple[float,float,float,float,Tuple[Tuple[int,int]]]:    # cost
         hashed = (i1, j1, i2, j2)
         if hashed in memo:
             if DEBUG: print(hashed, memo[hashed], "memo")
@@ -62,23 +64,23 @@ def TED(T1: Tree, T2: Tree,
 
         # Base cases
         if i1==j1 and i2==j2:   # forests are both empty
-            return 0, Counter({'INS': 0, 'DEL': 0, 'SUB': 0}), ()
+            return 0, 0, 0, 0, ()
         elif i2==j2: # 2nd forest is empty -> a deletion
             r1 = j1-1   # rightmost root of the T1 subforest
             subcall = _TED(i1, r1, i2, j2)
-            return (subcall[0] + DEL, subcall[1] + Counter({'DEL': DEL}), subcall[2])
+            return (subcall[0] + DEL, subcall[1], subcall[2]+DEL, subcall[3], subcall[4])
         elif i1==j1: # 1st forest is empty -> an insertion
             r2 = j2-1   # rightmost root of the T2 subforest
             subcall = _TED(i1, j1, i2, r2)
-            return (subcall[0] + INS, subcall[1] + Counter({'INS': INS}), subcall[2])
+            return (subcall[0] + INS, subcall[1]+INS, subcall[2], subcall[3], subcall[4])
 
         # Recursive case (both forests are nonempty)
         r1 = j1-1
         r2 = j2-1
         subcall = _TED(i1, r1, i2, j2)  # delete the rightmost tree of the T1 subforest
-        ted_DEL = (subcall[0] + DEL, subcall[1] + Counter({'DEL': DEL}), subcall[2])
+        ted_DEL = (subcall[0] + DEL, subcall[1], subcall[2]+DEL, subcall[3], subcall[4])
         subcall = _TED(i1, j1, i2, r2)  # insert the rightmost tree of the T2 subforest
-        ted_INS = (subcall[0] + INS, subcall[1] + Counter({'INS': INS}), subcall[2])
+        ted_INS = (subcall[0] + INS, subcall[1]+INS, subcall[2], subcall[3], subcall[4])
         # In the source article's notation,
         #    R_k is the subtree rooted at r_k, i.e. T_k[leftmost(r_k)...r_k]
         #    R_k - r_k is T[l(r_k)...r_k-1]
@@ -102,16 +104,21 @@ def TED(T1: Tree, T2: Tree,
             align_cost = SUB
 
         ted_REL = (subcallA[0] + align_cost + subcallB[0],
-                   subcallA[1] + Counter({'SUB': align_cost}) + subcallB[1],
-                   subcallA[2] + ((r1,r2),) + subcallB[2])
+                   subcallA[1]+subcallB[1], subcallA[2]+subcallB[2], subcallA[3]+subcallB[3]+align_cost,
+                   subcallA[4] + ((r1,r2),) + subcallB[4])
         memo[hashed] = min([ted_DEL, ted_INS, ted_REL])
         # TODO: tiebreaker other than the alignments?
 
         return memo[hashed]
 
-    cost, editcosts, alignments = _TED(0, len(nodes1), 0, len(nodes2))
-    alignments = [(f'{i1}:{T1.node_yield(nodes1[i1], gaps=True)}', f'{i2}:{T2.node_yield(nodes2[i2], gaps=True)}') for i1,i2 in alignments]
-    return cost, editcosts, tuple(alignments)
+    cost, INScost, DELcost, SUBcost, offset_alignments = _TED(0, len(nodes1), 0, len(nodes2))
+    editcosts = Counter({'INS': INScost, 'DEL': DELcost, 'SUB': SUBcost})
+    alignments = {nodes1[i1]: nodes2[i2] for i1,i2 in offset_alignments}
+    return cost, editcosts, alignments
+
+def stringify_alignments(alignments: Mapping[int,int], T1: Tree, T2: Tree) -> Tuple[Tuple[str,str]]:
+    result = [(f'{n1}:{T1.node_yield(n1, gaps=True)}', f'{n2}:{T2.node_yield(n2, gaps=True)}') for n1,n2 in alignments.items()]
+    return tuple(result)
 
 
 """
