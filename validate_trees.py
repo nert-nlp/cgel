f9251a4546d432bbefcf28956e12ad56f3318202
Nathan Schneider <nathan.schneider@georgetown.edu>
2023-06-11 20:30:26 -0400

    validate_trees.py: debug info

--- validate_trees.py
+++ validate_trees.py
@@ -9,6 +9,7 @@ from itertools import chain
 def main(cgelpaths):
     for cgelFP in cgelpaths:
         with open(cgelFP) as f:
+            print(cgelFP, file=sys.stderr)
             nFailures = 0
             for tree in cgel.trees(f, check_format=True):
                 # check_format=True ensures that the generated tree structure matches the input
@@ -22,6 +23,7 @@ def main(cgelpaths):
                     nWarn = _nWarn
                 except AssertionError as ex:
                     nFailures += 1
+                    print('catching AssertionError in validate_trees.py', file=sys.stderr)
                     print(ex, file=sys.stderr)
                     traceback.print_tb(ex.__traceback__, limit=1)
                     print("", file=sys.stderr)
