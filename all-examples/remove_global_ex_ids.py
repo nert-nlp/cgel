fdca99e75167a88ac7471e573e8612c7dde74a43
Nathan Schneider <nathan.schneider@georgetown.edu>
2024-02-27 10:24:10 -0500

    store YAML without global ex IDs to track content changes

---
+++ all-examples/remove_global_ex_ids.py
@@ -0,0 +1,38 @@
+import yaml
+import re
+import sys
+from typing import Mapping
+from collections import Counter
+
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+RE_GLOBAL_EX_ID = re.compile(r'^ex[0-9]+')
+
+def recurse(d: Mapping):
+    firstFullLocator = None
+    for k,v in d.items():
+        if k=='page': continue
+        if isinstance(v, list):
+            globalexid = v[0]
+            newexid = RE_GLOBAL_EX_ID.sub('', globalexid)
+            v[0] = newexid
+            if not firstFullLocator:
+                firstFullLocator = newexid
+        else:
+            return recurse(v)
+    return firstFullLocator
+
+
+with (open(sys.argv[1]) if sys.argv[1:] else sys.stdin) as inF:
+    doc = yaml.load(inF, Loader)
+    for k in list(doc.keys()):
+        globalexid = k
+        v = doc[globalexid]
+        newexid = recurse(v)
+        del doc[globalexid]
+        doc[newexid] = v
+
+print(yaml.dump(doc))
