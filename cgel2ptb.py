3cc52b4a021f18249ee9a9da5f4ffd411eb835fb
Brandon Waldon <32781952+bwaldon@users.noreply.github.com>
2024-08-02 22:50:20 -0400

    manual PUNCT switch for tree.ptb() call in cgel2ptb.py (#122)

--- cgel2ptb.py
+++ cgel2ptb.py
@@ -8,6 +8,7 @@ from itertools import chain
 import fileinput
 
 MODE = ['tags', 'trees'][1]
+PUNCT = [True, False][0]
 #with open('datasets/twitter.cgel') as f, open('datasets/ewt.cgel') as f2, open('datasets/ewt-new1-nschneid.cgel') as f3:
 for tree in cgel.trees(fileinput.input()):
-    print(tree.tagging(gap_symbol='_.') if MODE=='tags' else tree.ptb())
+    print(tree.tagging(gap_symbol='_.') if MODE=='tags' else tree.ptb(punct = PUNCT))
