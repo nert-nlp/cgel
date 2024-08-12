29b35f09b8df3b643ac7212b08b6b237cc5e7fcc
Brandon Waldon <32781952+bwaldon@users.noreply.github.com>
2024-08-12 08:56:55 -0700

    cgel2ptb.py: set  in call to tree.ptb()

--- cgel2ptb.py
+++ cgel2ptb.py
@@ -11,4 +11,4 @@ MODE = ['tags', 'trees'][1]
 PUNCT = [True, False][0]
 #with open('datasets/twitter.cgel') as f, open('datasets/ewt.cgel') as f2, open('datasets/ewt-new1-nschneid.cgel') as f3:
 for tree in cgel.trees(fileinput.input()):
-    print(tree.tagging(gap_symbol='_.') if MODE=='tags' else tree.ptb(punct = PUNCT))
+    print(tree.tagging(gap_symbol='_.') if MODE=='tags' else tree.ptb(punct = PUNCT, complex_lexeme_separator='_'))
